202d0ae7d031d9f63290e093e26d35bd497ba122
Luke Keltner <lkeltner87@gmail.com>
2017-08-07 16:16:37 -0400

    Learning about Event Delegation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,7 @@ $(document).ready(function()
 {
 	var userName;
 	var userTime;
+	var index;
 
 	var classTimes = ['Monday 1/1 7:00pm', 'Tuesday 1/2 4:00pm', 'Wednesday 1/3 6:30pm', 'Thursday 1/4 7:00pm']
 
@@ -9,6 +10,23 @@ $(document).ready(function()
 	var classOptionsTable = $('.class-options-table')
 
 	var students = ['Luke', 'Jason', 'Amanda']
+
+
+	function resetStudentList()
+	{
+		$('.student-container').html('<table class=\"table table-striped table-hover student-table\">')
+		$('.student-table').html('<th><h2>Who are You?</h2></th>')
+	}
+
+	function getCurrentStudents()
+	{
+		for (var i=0; i<students.length; i++)
+		{
+			var newStudent = $('<tr><td>'+students[i]+'</td></tr>')
+			newStudent.attr('class', 'user-name')
+			$('.student-table').append(newStudent)
+		}
+	}
 
 	for (var i=0; i<classTimes.length; i++)
 	{
@@ -20,17 +38,24 @@ $(document).ready(function()
 		classOptionsTable.append(inputClass)
 	}
 
-	for (var i=0; i<students.length; i++)
-	{
-		var newStudent = $('<tr><td>'+students[i]+'</td></tr>')
-		newStudent.attr('class', 'user-name')
-		$('.student-table').append(newStudent)
-	}
+	getCurrentStudents()
 
-	$('.user-name').on('click', function()
+	$('.submit').on('click', function()
+	{
+		students.splice(index, 1)
+		resetStudentList()
+		$('#'+userTime).append('<tr><td>'+userName+'</td></tr>')
+		console.log(students)
+		getCurrentStudents()
+	})
+
+	$(document).on('click', '.user-name', function()
 	{
     	userName = $(this).text()
+    	console.log(userName)
+    	index = students.indexOf(userName)
     	$('#confirm-name').html(userName)
+    	console.log(event)
 	});
 
 	$('.user-time').on('click', function()
@@ -40,14 +65,5 @@ $(document).ready(function()
     	$('#confirm-time').html(time)
     	console.log(userTime)
 	});
-
-	$('.submit').on('click', function()
-	{
-		$('#'+userTime).append('<tr><td>'+userName+'</td></tr>')
-	})
-
-
-
-
 
 })
